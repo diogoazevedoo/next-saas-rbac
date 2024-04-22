3ba441c26c1cc9a2cf2b28002dc0697fef41ebeb
Diogo Azevedo <idiogoazevedoo@gmail.com>
2024-04-22 16:34:14 +0100

    auth: start tests for rbac auth with casl library and create api app to test it

---
+++ apps/api/src/index.ts
@@ -0,0 +1,10 @@
+import { ability } from '@saas/auth'
+
+const userCanInviteSomeoneElse = ability.can('invite', 'User')
+const userCanDeleteOtherUsers = ability.can('delete', 'User')
+
+const userCannotDeleteOtherUsers = ability.cannot('delete', 'User')
+
+console.log(userCanInviteSomeoneElse)
+console.log(userCanDeleteOtherUsers)
+console.log(userCannotDeleteOtherUsers)
