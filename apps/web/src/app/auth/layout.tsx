0a23146bfa276501b9beebd21dc737aba3317fc9
Diogo Azevedo <idiogoazevedoo@gmail.com>
2024-09-10 22:46:44 +0100

    web: add sign-in implementation

--- apps/web/src/app/auth/layout.tsx
+++ apps/web/src/app/auth/layout.tsx
@@ -1,8 +1,16 @@
-export default function AuthLayout({
+import { redirect } from 'next/navigation'
+
+import { isAuthenticated } from '@/auth/auth'
+
+export default async function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  if (isAuthenticated()) {
+    redirect('/')
+  }
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center px-4">
       <div className="w-full max-w-xs">{children}</div>
---
+++ apps/web/src/app/auth/sign-in/actions.tsx
@@ -0,0 +1,54 @@
+'use server'
+
+import { HTTPError } from 'ky'
+import { cookies } from 'next/headers'
+import { z } from 'zod'
+
+import { signInWithPassword } from '@/http/sign-in-with-password'
+
+const signInSchema = z.object({
+  email: z
+    .string()
+    .email({ message: 'Please, provide a valid e-mail address.' }),
+  password: z.string().min(1, { message: 'Please, provide your password.' }),
+})
+
+export async function signInWithEmailAndPassword(data: FormData) {
+  const result = signInSchema.safeParse(Object.fromEntries(data))
+
+  if (!result.success) {
+    const errors = result.error.flatten().fieldErrors
+
+    return { success: false, message: null, errors }
+  }
+
+  const { email, password } = result.data
+
+  try {
+    const { token } = await signInWithPassword({
+      email,
+      password,
+    })
+
+    cookies().set('token', token, {
+      path: '/',
+      maxAge: 60 * 60 * 24 * 7, // 7 days
+    })
+  } catch (err) {
+    if (err instanceof HTTPError) {
+      const { message } = await err.response.json()
+
+      return { success: false, message, errors: null }
+    }
+
+    console.error(err)
+
+    return {
+      success: false,
+      message: 'Unexpected error, try again later.',
+      errors: null,
+    }
+  }
+
+  return { success: true, message: null, errors: null }
+}
