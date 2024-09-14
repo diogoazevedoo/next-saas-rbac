dcb1cc448dce6d7b6d258a932bf1284e10a625fd
Diogo Azevedo <idiogoazevedoo@gmail.com>
2024-09-14 20:09:41 +0100

    web: header component start (logo and user profile)

---
+++ apps/web/src/app/(app)/page.tsx
@@ -0,0 +1,10 @@
+import { Header } from '@/components/header'
+
+export default function Home() {
+  return (
+    <div className="py-4">
+      <Header />
+      <main></main>
+    </div>
+  )
+}
---
+++ apps/web/src/components/header.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+
+import logoIcon from '@/assets/logo-icon.svg'
+
+import { ProfileButton } from './profile-button'
+
+export function Header() {
+  return (
+    <div className="mx-auto flex max-w-[1200px] items-center justify-between">
+      <div className="flex items-center gap-3">
+        <Image src={logoIcon} className="size-6 dark:invert" alt="logo" />
+      </div>
+
+      <div className="flex items-center gap-4">
+        <ProfileButton />
+      </div>
+    </div>
+  )
+}
