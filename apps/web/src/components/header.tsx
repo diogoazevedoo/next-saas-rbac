df3688161fbfb2d6a5d976f2dee1dcbd048e51c7
Diogo Azevedo <idiogoazevedoo@gmail.com>
2024-09-15 20:58:45 +0100

    web: add organization switcher (ui)

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,7 +1,9 @@
+import { Slash } from 'lucide-react'
 import Image from 'next/image'
 
 import logoIcon from '@/assets/logo-icon.svg'
 
+import { OrganizationSwitcher } from './organization-switcher'
 import { ProfileButton } from './profile-button'
 
 export function Header() {
@@ -9,6 +11,10 @@ export function Header() {
     <div className="mx-auto flex max-w-[1200px] items-center justify-between">
       <div className="flex items-center gap-3">
         <Image src={logoIcon} className="size-6 dark:invert" alt="logo" />
+
+        <Slash className="text-border size-3 -rotate-[24deg]" />
+
+        <OrganizationSwitcher />
       </div>
 
       <div className="flex items-center gap-4">
---
+++ apps/web/src/components/organization-switcher.tsx
@@ -0,0 +1,48 @@
+import { ChevronsUpDown, PlusCircle } from 'lucide-react'
+import Link from 'next/link'
+
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
+
+export function OrganizationSwitcher() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="focus-visible:ring-primary flex w-[168px] items-center gap-2 rounded p-1 text-sm font-medium outline-none focus-visible:ring-2">
+        <span className="text-muted-foreground">Select organization</span>
+        <ChevronsUpDown className="text-muted-foreground ml-auto size-4" />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        align="end"
+        alignOffset={-16}
+        sideOffset={12}
+        className="w-[200px]"
+      >
+        <DropdownMenuGroup>
+          <DropdownMenuLabel>Organizations</DropdownMenuLabel>
+          <DropdownMenuItem>
+            <Avatar className="mr-2 size-4">
+              <AvatarImage src="https://github.com/diogoazevedoo.png" />
+              <AvatarFallback />
+            </Avatar>
+            <span className="line-clamp-1">Diogo's Team</span>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem asChild>
+          <Link href="/create-organization">
+            <PlusCircle className="mr-2 size-4" />
+            Create new
+          </Link>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
