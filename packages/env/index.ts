a32c662a23bed052a7e0fcd0f29674eb3f1030f4
Diogo Azevedo <idiogoazevedoo@gmail.com>
2024-10-06 18:40:31 +0100

    web: load env variables and fix theme switcher hydration bug

--- packages/env/index.ts
+++ packages/env/index.ts
@@ -12,7 +12,9 @@ export const env = createEnv({
     GITHUB_OAUTH_CLIENT_SECRET: z.string(),
     GITHUB_OAUTH_CLIENT_REDIRECT_URI: z.string().url(),
   },
-  client: {},
+  client: {
+    NEXT_PUBLIC_API_URL: z.string().url(),
+  },
   shared: {},
   runtimeEnv: {
     SERVER_PORT: process.env.SERVER_PORT,
@@ -22,6 +24,7 @@ export const env = createEnv({
     GITHUB_OAUTH_CLIENT_SECRET: process.env.GITHUB_OAUTH_CLIENT_SECRET,
     GITHUB_OAUTH_CLIENT_REDIRECT_URI:
       process.env.GITHUB_OAUTH_CLIENT_REDIRECT_URI,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
   emptyStringAsUndefined: true,
 })
