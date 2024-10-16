b99d0d23284a0e58cc7b22225906eee5dc1ce865
Diogo Azevedo <idiogoazevedoo@gmail.com>
2024-10-16 15:49:59 +0100

    web: create organization modal (sheet) with interception routes

--- apps/web/src/app/(app)/layout.tsx
+++ apps/web/src/app/(app)/layout.tsx
@@ -4,12 +4,19 @@ import { isAuthenticated } from '@/auth/auth'
 
 export default async function AppLayout({
   children,
+  sheet,
 }: Readonly<{
   children: React.ReactNode
+  sheet: React.ReactNode
 }>) {
   if (!isAuthenticated()) {
     redirect('/auth/sign-in')
   }
 
-  return <>{children}</>
+  return (
+    <>
+      {children}
+      {sheet}
+    </>
+  )
 }
