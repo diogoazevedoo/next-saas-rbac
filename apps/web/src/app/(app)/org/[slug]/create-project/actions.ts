8ea4d212cb81f838512eca0a4e75ec6a3f39ffdf
Diogo Azevedo <idiogoazevedoo@gmail.com>
2024-10-18 10:44:25 +0100

    web: create project

---
+++ apps/web/src/app/(app)/org/[slug]/create-project/actions.ts
@@ -0,0 +1,52 @@
+'use server'
+
+import { HTTPError } from 'ky'
+import { z } from 'zod'
+
+import { getCurrentOrg } from '@/auth/auth'
+import { createProject } from '@/http/create-project'
+
+const projectSchema = z.object({
+  name: z.string().min(4, { message: 'Please include at least 4 characters.' }),
+  description: z.string(),
+})
+
+export async function createProjectAction(data: FormData) {
+  const result = projectSchema.safeParse(Object.fromEntries(data))
+
+  if (!result.success) {
+    const errors = result.error.flatten().fieldErrors
+
+    return { success: false, message: null, errors }
+  }
+
+  const { name, description } = result.data
+
+  try {
+    await createProject({
+      org: getCurrentOrg()!,
+      name,
+      description,
+    })
+  } catch (err) {
+    if (err instanceof HTTPError) {
+      const { message } = await err.response.json()
+
+      return { success: false, message, errors: null }
+    }
+
+    console.error(err)
+
+    return {
+      success: false,
+      message: 'Unexpected error, try again later.',
+      errors: null,
+    }
+  }
+
+  return {
+    success: true,
+    message: 'Successfully saved the project.',
+    errors: null,
+  }
+}
