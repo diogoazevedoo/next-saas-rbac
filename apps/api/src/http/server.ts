ff6cf47d5040c7197fce9966d58494ca3698d461
Diogo Azevedo <idiogoazevedoo@gmail.com>
2024-10-23 15:19:32 +0100

    chore: setup default api host as 0.0.0.0

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -115,6 +115,6 @@ app.register(getPendingInvites)
 
 app.register(getOrganizationBilling)
 
-app.listen({ port: env.PORT }).then(() => {
+app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
   console.log('🔥 HTTP server running on port :3333')
 })
